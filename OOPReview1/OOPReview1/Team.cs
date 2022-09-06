e2ac6108ca1f0ba826ddee787f000fd3e3188cce
Matthew Suddaby <msuddaby2@nait.ca>
2022-09-06 14:44:59 -0600

    getters n setters

--- OOPReview1/OOPReview1/Team.cs
+++ OOPReview1/OOPReview1/Team.cs
@@ -22,9 +22,54 @@ namespace OOPReview1
 
     public class Team
     {
-        public Conference Conference { get; set; }
-        public Division Division { get; set; }
-        public string Name { get; set; }
+        private Conference Conference;
+        private Division Division;
+        private string Name;
+
+        public Team(Conference conference, Division division, string name)
+        {
+            Conference = conference;
+            Division = division;
+            Name = name;
+        }
+
+        public void SetConference(Conference conference)
+        {
+            Conference = conference;
+
+        }
+
+        public Conference GetConference()
+        {
+            return Conference;
+        }
+
+        public void SetDivision(Division division)
+        {
+            Division = division;
+        }
+
+        public Division GetDivision()
+        {
+            return Division;
+        }
+
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Name = name;
+            }
+            else
+            {
+                throw new Exception("You must enter a name for the team!");
+            }
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
 
     }
 }
