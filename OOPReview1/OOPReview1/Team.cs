ca377b2a5760c0d603987b2ebbeddb85774e204f
Matthew Suddaby <msuddaby2@nait.ca>
2022-09-06 14:36:00 -0600

    my nama cheffff

---
+++ OOPReview1/OOPReview1/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview1
+{
+    public enum Conference
+    {
+        EASTERN_CONFERENCE,
+        WESTERN_CONFERENCE
+    }
+
+    public enum Division
+    {
+        METROPOLITAN_DIVISION,
+        ATLANTIC_DIVISION,
+        CENTRAL_DIVISION,
+        PACIFIC_DIVISION
+    }
+
+    public class Team
+    {
+        public Conference Conference { get; set; }
+        public Division Division { get; set; }
+        public string Name { get; set; }
+
+    }
+}
