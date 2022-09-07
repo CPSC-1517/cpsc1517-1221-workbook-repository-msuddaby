b09342d1b67855f793654c405334043c6913a067
Matthew Suddaby <msuddaby2@nait.ca>
2022-09-07 16:26:02 -0600

    Wins / losses

--- OOPReview1/OOPReview1/NHLTeam.cs
+++ OOPReview1/OOPReview1/NHLTeam.cs
@@ -13,12 +13,20 @@ namespace OOPReview1
         private NHLConference _conference;
         private NHLDivision _division;
         private string _name;
+        private string _city;
+        private int _gamesPlayed;
+        private int _wins;
+        private int _losses;
+        private int _overtimeLosses;
+        private int _points;
+
         private List<NHLPlayer> _players;
-        public NHLTeam(NHLConference conference, NHLDivision division, string name)
+        public NHLTeam(NHLConference conference, NHLDivision division, string name, string city)
         {
             _conference = conference;
             _division = division;
             _name = name;
+            _city = city;
         }
 
         public NHLConference Conference
@@ -28,7 +36,110 @@ namespace OOPReview1
         public NHLDivision Division
         {
             get { return _division; }
-            set { _division = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Please enter a team name!");
+                }
+            }
+        }
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _city = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Please enter a city name!");
+                }
+            }
+        }
+
+        public int GamesPlayed { get { return _gamesPlayed; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _gamesPlayed = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Games played cannot be negative!");
+                }
+            }
+        }
+
+        public int Wins
+        {
+            get { return _wins; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _wins = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wins cannot be negative!");
+                }
+            }
+        }
+
+        public int Losses
+        {
+            get { return _losses; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _losses = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Losses cannot be negative!");
+                }
+            }
+        }
+
+        public int OvertimeLosses
+        {
+            get { return _overtimeLosses; }
+            set
+            {
+                if (value > 0)
+                {
+                    _overtimeLosses = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Overtime Losses cannot be negative!");
+                }
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                _points = 0;
+                _points += _wins * 2;
+                _points += _overtimeLosses;
+
+                return _points; 
+            }
         }
 
         public List<NHLPlayer> Players
--- OOPReview1/OOPReview1/Program.cs
+++ OOPReview1/OOPReview1/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            NHLTeam newTeam = new NHLTeam(NHLConference.WESTERN, NHLDivision.METROPOLITAN, "Bruh Moments", "Edmonton");
+
+            newTeam.Wins = 28;
+            newTeam.OvertimeLosses = 5;
+
+            Console.WriteLine(newTeam.Points);
         }
     }
 }
