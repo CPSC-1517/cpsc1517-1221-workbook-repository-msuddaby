0ff46a7725305dd20571e8ff0e4ed02f64285a36
Matthew Suddaby <msuddaby2@nait.ca>
2022-09-07 15:56:48 -0600

    woah

---
+++ OOPReview1/OOPReview1/NHLPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview1
+{
+    public class NHLPlayer
+    {
+        private int _playerNumber;
+        private string _playerName;
+        private NHLTeam _playerTeam;
+
+        public NHLPlayer (int playerNumber, string playerName, NHLTeam playerTeam)
+        {
+            _playerNumber = playerNumber;
+            _playerName = playerName;
+            _playerTeam = playerTeam;
+        }
+
+        public int PlayerNumber
+        {
+            get { return _playerNumber; }
+            set { _playerNumber = value; }
+        }
+
+        public string PlayerName
+        {
+            get { return _playerName; }
+            set { _playerName = value; }
+        }
+
+        public NHLTeam PlayerTeam
+        {
+            get { return _playerTeam; }
+            set { _playerTeam = value; }
+        }
+
+
+    }
+}
---
+++ OOPReview1/OOPReview1/NHLTeam.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview1
+{
+
+
+    public class NHLTeam
+    {
+        private NHLConference _conference;
+        private NHLDivision _division;
+        private string _name;
+        private List<NHLPlayer> _players;
+        public NHLTeam(NHLConference conference, NHLDivision division, string name)
+        {
+            _conference = conference;
+            _division = division;
+            _name = name;
+        }
+
+        public NHLConference Conference
+        {
+            get { return _conference; }
+        }
+        public NHLDivision Division
+        {
+            get { return _division; }
+            set { _division = value; }
+        }
+
+        public List<NHLPlayer> Players
+        {
+            get { return _players; }
+        }
+
+        public void AddPlayer(NHLPlayer player)
+        {
+            _players.Add(player);
+        }
+
+    }
+}
