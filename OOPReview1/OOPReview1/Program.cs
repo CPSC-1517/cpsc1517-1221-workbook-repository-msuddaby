0708231784946f32950d9a0c8d928256581e7753
Matthew Suddaby <msuddaby2@nait.ca>
2022-09-07 16:52:55 -0600

    printing and creating objects (vice-reversa)

--- OOPReview1/OOPReview1/Program.cs
+++ OOPReview1/OOPReview1/Program.cs
@@ -5,11 +5,30 @@
         static void Main(string[] args)
         {
             NHLTeam newTeam = new NHLTeam(NHLConference.WESTERN, NHLDivision.METROPOLITAN, "Bruh Moments", "Edmonton");
+            NHLTeam newTeam2 = new NHLTeam(NHLConference.EASTERN, NHLDivision.METROPOLITAN, "Lightning", "IDK");
 
-            newTeam.Wins = 28;
-            newTeam.OvertimeLosses = 5;
+            newTeam.Wins = 33;
+            newTeam.Losses = 42;
+            newTeam.OvertimeLosses = 7;
 
-            Console.WriteLine(newTeam.Points);
+            newTeam2.Wins = 51;
+            newTeam2.Losses = 23;
+            newTeam2.OvertimeLosses = 8;
+
+            Console.WriteLine($"TEAM 1:\n" +
+                $"\tName - {newTeam.Name}" +
+                $"\tWins - {newTeam.Wins}" +
+                $"\tLosses - {newTeam.Losses}" +
+                $"\tOvertime Losses - {newTeam.OvertimeLosses}" +
+                $"\tPOINTS - {newTeam.Points}");
+
+            Console.WriteLine($"TEAM 2:\n" +
+                $"\tName - {newTeam2.Name}" +
+                $"\tWins - {newTeam2.Wins}" +
+                $"\tLosses - {newTeam2.Losses}" +
+                $"\tOvertime Losses - {newTeam2.OvertimeLosses}" +
+                $"\tPOINTS - {newTeam2.Points}");
+
         }
     }
 }
